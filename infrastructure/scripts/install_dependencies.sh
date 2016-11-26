bcbe78456b8854422ac84d5be11d1fb14c30d38b
Jared Wasinger <j-wasinger@hotmail.com>
2016-11-26 15:55:24 -0500

    update travis

--- infrastructure/scripts/install_dependencies.sh
+++ infrastructure/scripts/install_dependencies.sh
@@ -1,2 +1,4 @@
 #! /bin/bash
-yum install -y nodejs-0.10.46-1.el6
+yum install -y nodejs-0.10.46-1.el6 npm-1.3.6-5.el6 git.x86_64-2.7.4-1.47.amzn1
+bower install --allow-root ../..
+npm install ../..
