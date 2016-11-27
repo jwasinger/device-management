f721e7ff446b05b2cdfa0cdb3f55eb3eee97882e
Jared Wasinger <j-wasinger@hotmail.com>
2016-11-26 19:03:54 -0500

    .

--- infrastructure/scripts/install_dependencies.sh
+++ infrastructure/scripts/install_dependencies.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
-cd /home/ec2-user/app/web_app
 
 yum install -y nodejs-0.10.46-1.el6 npm-1.3.6-5.el6 git-2.7.4-1.47.amzn1
-bower install --allow-root
-npm install 
+bower install --allow-root ../../web_app
+npm install ../../web_app
