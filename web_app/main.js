0de4ae7c832fd8ee3bcd2553e42bc85727dbb095
j-wasinger@hotmail.com <j-wasinger@hotmail.com>
2017-02-26 13:53:26 -0800

    visual updates

--- web_app/main.js
+++ web_app/main.js
@@ -16,5 +16,5 @@ nunjucks.configure(path.join(__dirname, './public/templates'), {
 });
 
 express_app.listen(3000, function() {
-  console.log('server started');
+  console.log('server started on port 3000');
 });
--- web_app/routes.js
+++ web_app/routes.js
@@ -6,4 +6,8 @@ router.get('/dashboard', function(req, res) {
   res.render('index.html');
 });
 
+router.get('/', function(req, res) {
+  res.render('index.html');
+});
+
 module.exports = router;
