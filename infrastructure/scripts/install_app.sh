b54009e40815a08abf51806ce9b3b7b4fb58f0c0
Jared Wasinger <j-wasinger@hotmail.com>
2017-02-26 17:33:18 -0500

    update deps

--- infrastructure/scripts/install_app.sh
+++ infrastructure/scripts/install_app.sh
@@ -4,3 +4,4 @@ cp infrastructure/config/epel.repo /etc/yum.repos.d/
 yum install -y nodejs-0.10.46-1.el6 npm-1.3.6-5.el6 git-2.7.4-1.47.amzn1
 bower install --allow-root web_app
 npm install web_app
+echo "DEPENDENCIES INSTALLED..."
--- infrastructure/scripts/install_dependencies.sh
+++ infrastructure/scripts/install_dependencies.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 cur_dir=$(dirname "$0")
+echo $(pwd) &> build-output.txt
 
 $cur_dir/install_app.sh &> build-output.txt
