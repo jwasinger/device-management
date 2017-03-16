c1ad4c714d0d6d0b74a7568c6d7de5ee472732d9
j-wasinger@hotmail.com <j-wasinger@hotmail.com>
2017-03-15 21:15:11 -0700

    fix app install issue

--- infrastructure/scripts/install_app.sh
+++ infrastructure/scripts/install_app.sh
@@ -11,4 +11,4 @@ chown -R web-app:web-app /app/
 #remove the existing service file (if it exists)
 rm -f /usr/lib/systemd/system/material-dashboard.service
 
-cp /infrastructure/config/material-dashboard.service /usr/lib/systemd/system
+cp /app/infrastructure/config/material-dashboard.service /usr/lib/systemd/system
